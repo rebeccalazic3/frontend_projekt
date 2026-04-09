9cf42c6d47694313777c409b7839cd0f4e87a9c0
MBucaj <143554778+MBucaj@users.noreply.github.com>
2026-04-09 13:13:45 +0200

    a

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: 'backendprojekt-production.up.railway.app',
+  baseURL: 'https://backendprojekt-production.up.railway.app',
   timeout: 5000,
 })
 
